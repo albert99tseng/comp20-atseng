99fe9e7e134ba97b3ff33e216aff6966cef70066
Albert Tseng <albert99tseng@gmail.com>
2018-10-23 01:43:13 -0400

    updated files

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -41,8 +41,6 @@ var Ashmont_polyline = [
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), option);
     myLocation();
-    setMarker();
-    getPolyline();
 }
 
 function myLocation(){
@@ -70,6 +68,8 @@ function renderMap(){
 			else{
 				var timedata = JSON.parse(request.responseText);
 				showTrainData(timedata);
+				setMarker();
+    			getPolyline();
 			}
 		}
 	}
@@ -267,40 +267,33 @@ function getPolyline(){
         });
         Braintree_path.setMap(map);
 }
-/*
-function myLocation(){
-	if (navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position){
-			curr_lat = position.coords.latitude;
-			curr_long = position.coords.longitude;
-			renderMap();
-		});
-	}
-	else{
-		alert("Geolocation is not supported.")
-	}
+
+//http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
+function haversineDistance(coords1, coords2, isMiles) {
+        function toRad(x) {
+                return x * Math.PI / 180;
+        }
+
+        var lon1 = coords1[0];
+        var lat1 = coords1[1];
+
+        var lon2 = coords2[0];
+        var lat2 = coords2[1];
+
+        var R = 6371; // km
+
+        var x1 = lat2 - lat1;
+        var dLat = toRad(x1);
+        var x2 = lon2 - lon1;
+        var dLon = toRad(x2)
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+
+        if(isMiles) d /= 1.60934;
+
+        return d;
 }
-
-
-function renderMap(){
-	request.open('GET', "https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id=place-davis", true);
-	request.send();
-	request.onreadystatechange = function(){
-		if (request.readyState == XHMLHttpRequest.DONE){
-			if (request.readyState != 200){
-				alert("Can't load train data")
-			}
-			else{
-				var timedata = JSON.parse(request.responseText);
-				//showTrainData(timedata);
-			}
-		}
-	}
-}
-
-*/
-
-
 
 
 /*
